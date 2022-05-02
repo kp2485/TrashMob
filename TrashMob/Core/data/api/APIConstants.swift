1f938c09aacf1135ace3e4b57c4d01a0912dc5ab
Kyle Peterson <kdpeters@gmail.com>
2022-05-02 08:48:06 -0400

    api credential file created

---
+++ TrashMob/Core/data/api/APIConstants.swift
@@ -0,0 +1,15 @@
+//
+//  APIConstants.swift
+//  TrashMob
+//
+//  Created by Kyle Peterson on 5/2/22.
+//
+
+import Foundation
+
+enum APIConstants {
+  static let host = "api.earth911.com"
+  static let grantType = "client_credentials"
+  static let clientId = "YourKeyHere"
+  static let clientSecret = "YourSecretHere"
+}
