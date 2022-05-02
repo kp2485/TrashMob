24a52f9b593b5c61ecbb78ed1c67ad8b36dc9c91
Kyle Peterson <kdpeters@gmail.com>
2022-05-02 11:50:00 -0400

    committed

---
+++ TrashMob/TrashMobsNearYou/views/TrashMobsNearYouView.swift
@@ -0,0 +1,23 @@
+//
+//  TrashMobsNearYouView.swift
+//  TrashMob
+//
+//  Created by Kyle Peterson on 5/2/22.
+//
+
+import SwiftUI
+
+struct TrashMobsNearYouView: View {
+    var body: some View {
+        NavigationView {
+          Text("TODO: Trash Mobs Near You View")
+            .navigationTitle("Trash mobs near you")
+        }.navigationViewStyle(StackNavigationViewStyle())
+    }
+}
+
+struct TrashMobsNearYouView_Previews: PreviewProvider {
+    static var previews: some View {
+        TrashMobsNearYouView()
+    }
+}
