7b6f6f52ff55ae4e63e686b7b1478833abeaad4c
Kyle Peterson <kdpeters@gmail.com>
2022-05-02 09:11:08 -0400

    building out coreData structures

---
+++ TrashMob/Core/model/organization/Organization.swift
@@ -0,0 +1,31 @@
+//
+//  Organization.swift
+//  TrashMob
+//
+//  Created by Kyle Peterson on 5/2/22.
+//
+
+import Foundation
+
+struct Organization: Codable {
+  var id: Int?
+  var contact: Contact?
+  var distance: Double?
+}
+
+struct Contact: Codable {
+  var id: Int?
+  var email: String?
+  var phone: String?
+  var address: Address?
+}
+
+struct Address: Codable {
+  var id: Int?
+  var address1: String?
+  var address2: String?
+  var city: String?
+  var state: String?
+  var postcode: String?
+  var country: String?
+}
