4f6f11553e4ad1bbc73c3c6f9533021d6b219fb6
Kyle Peterson <kdpeters@gmail.com>
2022-05-02 11:37:19 -0400

    search feature buildout

---
+++ TrashMob/Search/views/SearchView.swift
@@ -0,0 +1,21 @@
+//
+//  SearchView.swift
+//  TrashMob
+//
+//  Created by Kyle Peterson on 5/2/22.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text("TODO: Search View")
+          .navigationTitle("Find a trash mob")
+    }
+}
+
+struct SearchView_Previews: PreviewProvider {
+    static var previews: some View {
+        SearchView()
+    }
+}
