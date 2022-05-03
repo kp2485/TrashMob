764a1910c87e6d92baa511e67350a888d7161a2a
Kyle Peterson <kdpeters@gmail.com>
2022-05-03 09:54:59 -0400

    initial commit trashMob struct

--- TrashMob/Core/model/trashMob/trashMob.swift
+++ TrashMob/Core/model/trashMob/trashMob.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+struct TrashMob: Codable {
+    var id = UUID()
+    var name: String
+    var creationDate: Date
+    var mobDate: Date
+    var status: String
+    var type: String
+    var suppliesNeeded: [String]
+    var suppliesVolunteered: [String]
+    var likes: Int
+    var comments: [String:String]
+}
