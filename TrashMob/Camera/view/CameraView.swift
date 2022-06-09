8cde9796a12bb03f9106616b26cc352981ebe0a4
Kyle Peterson <kdpeters@gmail.com>
2022-06-09 15:47:12 -0400

    camera navigation

--- TrashMob/Camera/view/CameraView.swift
+++ TrashMob/Camera/view/CameraView.swift
@@ -12,9 +12,20 @@ struct CameraView: View {
     
     @State var currentZoomFactor: CGFloat = 1.0
     
+    //    var hasPhoto: Binding<Bool> {
+    //        if model.photo != nil {
+    //            return .constant(true)
+    //        } else {
+    //            return .constant(false)
+    //        }
+    //    }
+    
+    @State private var hasPhoto: Bool = false
+    
     var captureButton: some View {
         Button(action: {
             model.capturePhoto()
+            hasPhoto = true
         }, label: {
             Circle()
                 .foregroundColor(.white)
@@ -35,7 +46,7 @@ struct CameraView: View {
                     .aspectRatio(contentMode: .fill)
                     .frame(width: 60, height: 60)
                     .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
-//                    .animation(.spring())
+                //                    .animation(.spring())
                 
             } else {
                 RoundedRectangle(cornerRadius: 10)
@@ -59,68 +70,75 @@ struct CameraView: View {
     }
     
     var body: some View {
-        GeometryReader { reader in
-            ZStack {
-                Color.black.edgesIgnoringSafeArea(.all)
-                
-                VStack {
-                    Button(action: {
-                        model.switchFlash()
-                    }, label: {
-                        Image(systemName: model.isFlashOn ? "bolt.fill" : "bolt.slash.fill")
-                            .font(.system(size: 20, weight: .medium, design: .default))
-                    })
-                    .accentColor(model.isFlashOn ? .yellow : .white)
-                    
-                    CameraPreview(session: model.session)
-                        .gesture(
-                            DragGesture().onChanged({ (val) in
-                                //  Only accept vertical drag
-                                if abs(val.translation.height) > abs(val.translation.width) {
-                                    //  Get the percentage of vertical screen space covered by drag
-                                    let percentage: CGFloat = -(val.translation.height / reader.size.height)
-                                    //  Calculate new zoom factor
-                                    let calc = currentZoomFactor + percentage
-                                    //  Limit zoom factor to a maximum of 5x and a minimum of 1x
-                                    let zoomFactor: CGFloat = min(max(calc, 1), 5)
-                                    //  Store the newly calculated zoom factor
-                                    currentZoomFactor = zoomFactor
-                                    //  Sets the zoom factor to the capture device session
-                                    model.zoom(with: zoomFactor)
-                                }
-                            })
-                        )
-                        .onAppear {
-                            model.configure()
-                        }
-                        .alert(isPresented: $model.showAlertError, content: {
-                            Alert(title: Text(model.alertError.title), message: Text(model.alertError.message), dismissButton: .default(Text(model.alertError.primaryButtonTitle), action: {
-                                model.alertError.primaryAction?()
-                            }))
-                        })
-//                        .overlay(
-//                            Group {
-//                                if model.willCapturePhoto {
-//                                    Color.black
-//                                }
-//                            }
-//                        )
-//                        .animation(.easeInOut)
-                    
-                    
-                    HStack {
-                        capturedPhotoThumbnail
-                        
-                        Spacer()
-                        
-                        captureButton
-                        
-                        Spacer()
-                        
-                        flipCameraButton
-                        
+        NavigationView {
+            GeometryReader { reader in
+                ZStack {
+                    NavigationLink(isActive: $hasPhoto) {
+                        TargetATrashMob()
+                    } label: {
+                        EmptyView()
                     }
-                    .padding(.horizontal, 20)
+                    Color.black.edgesIgnoringSafeArea(.all)
+                    
+                    VStack {
+                        Button(action: {
+                            model.switchFlash()
+                        }, label: {
+                            Image(systemName: model.isFlashOn ? "bolt.fill" : "bolt.slash.fill")
+                                .font(.system(size: 20, weight: .medium, design: .default))
+                        })
+                        .accentColor(model.isFlashOn ? .yellow : .white)
+                        
+                        CameraPreview(session: model.session)
+                            .gesture(
+                                DragGesture().onChanged({ (val) in
+                                    //  Only accept vertical drag
+                                    if abs(val.translation.height) > abs(val.translation.width) {
+                                        //  Get the percentage of vertical screen space covered by drag
+                                        let percentage: CGFloat = -(val.translation.height / reader.size.height)
+                                        //  Calculate new zoom factor
+                                        let calc = currentZoomFactor + percentage
+                                        //  Limit zoom factor to a maximum of 5x and a minimum of 1x
+                                        let zoomFactor: CGFloat = min(max(calc, 1), 5)
+                                        //  Store the newly calculated zoom factor
+                                        currentZoomFactor = zoomFactor
+                                        //  Sets the zoom factor to the capture device session
+                                        model.zoom(with: zoomFactor)
+                                    }
+                                })
+                            )
+                            .onAppear {
+                                model.configure()
+                            }
+                            .alert(isPresented: $model.showAlertError, content: {
+                                Alert(title: Text(model.alertError.title), message: Text(model.alertError.message), dismissButton: .default(Text(model.alertError.primaryButtonTitle), action: {
+                                    model.alertError.primaryAction?()
+                                }))
+                            })
+                        //                        .overlay(
+                        //                            Group {
+                        //                                if model.willCapturePhoto {
+                        //                                    Color.black
+                        //                                }
+                        //                            }
+                        //                        )
+                        //                        .animation(.easeInOut)
+                        
+                        
+                        HStack {
+                            capturedPhotoThumbnail
+                            
+                            Spacer()
+                            
+                            captureButton
+                            
+                            Spacer()
+                            
+                            flipCameraButton
+                            
+                        }
+                        .padding(.horizontal, 20)
+                    }
                 }
             }
         }
